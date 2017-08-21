30726ebba2afdf114dc0a325204eeccea2d26fe5
Evgeny Oyun <evgeny.oyun@gmail.com>
2017-08-21 23:15:19 +0300

    Create Request.cpp

---
+++ src/Http/Parser/Request.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <functional>
+#include <exception>
+#include <stdexcept>
+
+#include "Base.cpp"
+
+#ifndef _HTTP_PARSER_REQUEST_C
+#define _HTTP_PARSER_REQUEST_C
+
+namespace Http
+{
+  namespace Parser
+  {
+    using namespace std;
+
+    class Request{
+    public:
+      static void parse(const string *raw, function<void(const string*, const string*)> &handler)
+      {
+        Base(raw, ':', '\n', handler);
+      }
+    };
+  }
+}
+
+#endif // _HTTP_PARSER_REQUEST_C
