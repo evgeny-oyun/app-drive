c3a7c79c725656d8374f52bf7ee9a9a655470824
Evgeny Oyun <evgeny.oyun@gmail.com>
2017-08-21 23:13:23 +0300

    Update Cookie.cpp

--- src/Http/Parser/Cookie.cpp
+++ src/Http/Parser/Cookie.cpp
@@ -19,7 +19,7 @@ namespace Http
     public:
       static void parse(const string *raw, function<void(const string*, const string*)> &handler)
       {
-        Base(raw, ';', handler);
+        Base(raw, '=', ';', handler);
       }
     };
   }
