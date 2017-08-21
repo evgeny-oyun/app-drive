1402a3aba916325b4fc95b7f9f4f6d179a5680eb
Evgeny Oyun <evgeny.oyun@gmail.com>
2017-08-21 23:04:21 +0300

    Update Cookie.cpp

--- src/Http/Parser/Cookie.cpp
+++ src/Http/Parser/Cookie.cpp
@@ -4,7 +4,7 @@
 #include <exception>
 #include <stdexcept>
 
-#include "../Exception/Exception.hpp"
+#include "Base.cpp"
 
 #ifndef _HTTP_PARSER_COOKIE_C
 #define _HTTP_PARSER_COOKIE_C
@@ -15,106 +15,11 @@ namespace Http
   {
     using namespace std;
 
-    typedef function<void(const string*, const string*)> cookie_callback_t;
-
     class Cookie{
     public:
-      static void parse(const string *raw, const cookie_callback_t &handler)
+      static void parse(const string *raw, function<void(const string*, const string*)> &handler)
       {
-        if(raw == NULL || raw->size() == 0)
-        {
-          return;
-        }
-
-        //
-        enum state_t{
-            STATE_KEY_INIT  = 0,
-            STATE_KEY_START = 1,
-            STATE_KEY_END   = 2
-        };
-
-        state_t state  = STATE_KEY_INIT;
-        size_t  i      = 0,
-                offset = 0,
-                length = 0,
-                max    = raw->size() - 1;
-
-        string           key, value;
-
-        for(i = 0; i<= max; i++)
-        {
-          if(raw->at(i) == '\t' || raw->at(i) == '\r' || raw->at(i) == '\n')
-          {
-            throw Http::Exception("A cookie parsing error: the string you given contains forbidden char");
-            return;
-          }
-
-          switch(state)
-          {
-            case STATE_KEY_INIT: {
-              if(raw->at(i) == ' ' || raw->at(i) == ';')
-              {
-                offset++;
-                continue;
-              }
-
-              state = STATE_KEY_START;
-              length++;
-            }
-            break;
-            case STATE_KEY_START:{
-              if(raw->at(i) == ' ')
-              {
-                offset++;
-                continue;
-              }
-
-              if(raw->at(i) == '=')
-              {
-                key = raw->substr(offset, length);
-
-                state   = STATE_KEY_END;
-                offset  = i;
-                length  = 0;
-
-                continue;
-              }
-
-              length++;
-            }
-            break;
-            case STATE_KEY_END:{
-              if(raw->at(i) == ' ')
-              {
-                offset++;
-                continue;
-              }
-
-              if(raw->at(i) == ';' || i == max)
-              {
-                offset++;
-
-                if(i == max && raw->at(max) != ';')
-                {
-                  length++;
-                }
-
-                value  = raw->substr(offset, length);
-
-                handler(&key, &value);
-
-                length = 0;
-                offset = i + 1;
-                state  = STATE_KEY_INIT;
-
-                continue;
-              }
-
-              length++;
-            }
-            break;
-          }
-        }
+        Base(raw, ';', handler);
       }
     };
   }
